165b635e3ebc736eb96563a775c0bbfb21936a20
Trey Love <theendurinpupil@gmail.com>
2021-04-26 23:17:49 -0700

    added the db functionality to index.js

--- index.js
+++ index.js
@@ -1,63 +1,87 @@
-const inquirer = require(inquirer)
-const mysql = require(mysql)
+const inquirer = require('inquirer')
+const mysql = require('mysql')
 
-const mainMen = () => {
-    inquirer.prompt({
-        name: 'logDb',
-        type: 'list',
-        message: 'what would you like to do?',
-        choices: ['View all employees',
-            'View all employees by manager',
-            'View all employees by department',
-            'View all employees by role',
-            'Add employee',
-            'Add department',
-            'Add roles',
-            'Update employee roles']
-    })
-        .then((answer) => {
-            switch (answer.logDb) {
-                case 'View all employees':
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employee_db'
+});
 
-                    break;
+connection.connect()
+const updateRole = () => {
+    console.log('is this working');
+    inquirer.prompt([
+        {
+            name: 'empId',
+            type: 'input',
+            message: 'Insert employee ID:'
 
-                case 'View all employees by manager':
 
-                    break;
+        },
+        {
+            name: 'empRole',
+            type: 'input',
+            message: "New employee role ID:"
+        }
+    ])
 
-                case 'View all employees by department':
+        .then((answers) => {
+            connection.query(
+                'UPDATE employee SET roleId = ? WHERE id = ?',
+                [answers.empRole, answers.empId], (err) => {
+                    if (err) throw err
+                    mainMen();
+                }
 
-                    break;
+            )
 
-                case 'View all employees by role':
-
-                    break;
-
-                case 'Add employee':
-
-                    break;
-
-                case 'Add department':
-
-                    break;
-
-                case 'Add roles':
-
-                    break;
-
-                case 'Update employee roles':
-
-                    break;
-            }
         })
 }
 
+
+//connection.query(
+//'UPDATE employee SET ? WHERE?',
+//[{managerId: answers.empMan}, {id: answers.empId}]
+//)
+
+const viewEmp = () => {
+    connection.query(
+        'SELECT employee.id, employee.firstName, employee.lastName, employee.managerId, role.title AS Role, department.name AS Department  FROM employee LEFT JOIN role ON employee.roleId = role.id LEFT JOIN department ON role.departmentId = department.id', function (err, data) {
+            console.table(data)
+            mainMen();
+            if (err) throw err
+        }
+    )
+}
+const viewDep = () => {
+    connection.query(
+        'SELECT * FROM department', function (err, data) {
+            console.table(data)
+            mainMen();
+            if (err) throw err
+        }
+    )
+}
+const viewRole = () => {
+    connection.query(
+        'SELECT * FROM role', function (err, data) {
+            console.table(data)
+            mainMen();
+            if (err) throw err
+        }
+    )
+}
 const addEmp = () => {
-    inquirer.prompt({
-        name: 'firstName',
-        type: 'input',
-        message: 'Employee first name:',
-    },
+
+
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'Employee first name:'
+        },
         {
             name: 'lastName',
             type: 'input',
@@ -66,24 +90,48 @@ const addEmp = () => {
         {
             name: 'role',
             type: 'input',
-            message: 'Employee role'
+            message: 'Employee role id:'
         },
         {
             name: 'empMan',
             type: 'input',
-            message: 'Employee manager'
-        })
-        .then(answer){
+            message: 'Employee manager id:'
+        }])
 
-    }
+
+
+        .then((answer) => {
+            connection.query(
+                'INSERT INTO employee SET ?',
+
+                {
+                    firstName: answer.firstName,
+                    lastName: answer.lastName,
+                    roleId: answer.role,
+                    managerId: answer.empMan,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your new employee was added!');
+
+                    mainMen();
+                }
+            );
+
+        })
 }
 
 const addRole = () => {
-    inquirer.prompt({
-        name: 'newRole',
-        type: 'input',
-        message: 'Name of new role:'
-    },
+
+
+
+
+    inquirer.prompt([
+        {
+            name: 'newRole',
+            type: 'input',
+            message: 'Name of new role:'
+        },
         {
             name: 'roleSal',
             type: 'input',
@@ -93,21 +141,150 @@ const addRole = () => {
             name: 'roleDep',
             type: 'input',
             message: 'Role department:'
-        })
-        .then(answer){
+        }
+    ])
+        .then((answer) => {
+            connection.query(
+                'INSERT INTO role SET ?',
 
-    }
+                {
+                    title: answer.newRole,
+                    salary: answer.roleSal,
+                    departmentId: answer.roleDep,
+
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your new role was added!');
+
+                    mainMen();
+                }
+            );
+
+        })
 }
 
 const addDep = () => {
-    inquirer.prompt({
-        name: 'newDep',
-        type: 'input',
-        message: 'Name of new department:'
+    //viewDep();
 
-    })
-        .then(answer){
+    inquirer.prompt([
+        {
+            name: 'newDep',
+            type: 'input',
+            message: 'Name of new department:'
 
-    }
+        }
+    ])
+        .then((answer) => {
+            connection.query(
+                'INSERT INTO department SET ?',
+
+                {
+                    name: answer.newDep
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your new department was added!');
+
+                    mainMen();
+                }
+            );
+
+        })
 }
 
+const mainMen = () => {
+    inquirer.prompt({
+        name: 'logDb',
+        type: 'list',
+        message: 'what would you like to do?',
+        choices: ['View', 'Add', 'Remove', 'Update']
+    })
+        .then((answer) => {
+            switch (answer.logDb) {
+                case 'View':
+                    inquirer.prompt({
+                        name: 'viewChoice',
+                        type: 'list',
+                        message: 'What would you like to view?',
+                        choices: ['Employees', 'Departments', 'Roles']
+                    })
+                        .then((answer) => {
+                            if (answer.viewChoice === 'Employees') {
+                                viewEmp()
+                            }
+                            else if (answer.viewChoice === 'Departments') {
+                                viewDep()
+                            }
+                            else if (answer.viewChoice === 'Roles') {
+                                viewRole()
+                            }
+                        })
+
+                    break;
+
+                case 'Add':
+                    inquirer.prompt({
+                        name: 'addChoice',
+                        type: 'list',
+                        message: 'What would you like to add?',
+                        choices: ['Employees', 'Departments', 'Roles']
+                    })
+                        .then((answer) => {
+                            if (answer.addChoice === 'Employees') {
+                                addEmp()
+                            }
+                            else if (answer.addChoice === 'Departments') {
+                                addDep()
+                            }
+                            else if (answer.addChoice === 'Roles') {
+                                addRole()
+                            }
+                        })
+                    break;
+
+                case 'Remove':
+                    inquirer.prompt({
+                        name: 'remChoice',
+                        type: 'list',
+                        message: 'What would you like to remove?',
+                        choices: ['Employees', 'Departments', 'Roles']
+                    })
+                        .then((answer) => {
+                            if (answer.remChoice === 'Employees') {
+
+                            }
+
+                            else if (answer.remChoice === 'Departments') {
+
+                            }
+
+                            else if (answer.remChoice === 'Roles') {
+
+                            }
+                        })
+
+                    break;
+
+                case 'Update':
+                    inquirer.prompt({
+                        name: 'updateChoice',
+                        type: 'list',
+                        message: 'What would you like to update',
+                        choices: ['Employee Role']
+                    })
+                        .then((answer) => {
+                            if (answer.updateChoice === 'Employee Role') {
+                                updateRole()
+                            }
+
+                        })
+
+
+                    break;
+
+            }
+        })
+}
+
+mainMen()
